37c42426d2e46a9c29effdda612ee27f97f173e2
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-07-21 02:10:58 +0300

    feat: add method and additional options arguments for model construction

--- model_compiler/model_compiler.cpp
+++ model_compiler/model_compiler.cpp
@@ -1,22 +1,29 @@
 #include "model_compiler.h"
 
 #include "data_extractor.h"
+#include "io_operations.h"
 #include "model_constructor.h"
 
-auto mc::make_surface_model(const std::string &folder, const float step_length)
-    -> void {
-  const auto cloud = data_extractor::form_surface(folder, step_length);
-
-  const model_constructor constructor{
-      model_constructor::methods::advancing_front};
-  const auto mesh = constructor.make_mesh(cloud);
-}
-
 auto mc::make_rotation_model(const std::string &folder,
+                             const model_constructor::methods method,
+                             const model_constructor::additional_options option,
                              const unsigned step_count) -> void {
   const auto cloud = data_extractor::form_rotation(folder, step_count);
 
-  const model_constructor constructor{
-      model_constructor::methods::advancing_front};
+  const model_constructor constructor{method, option};
   const auto mesh = constructor.make_mesh(cloud);
+
+  io::write_mesh(mesh);
+}
+
+auto mc::make_surface_model(const std::string &folder,
+                            const model_constructor::methods method,
+                            const model_constructor::additional_options option,
+                            const float step_length) -> void {
+  const auto cloud = data_extractor::form_surface(folder, step_length);
+
+  const model_constructor constructor{method, option};
+  const auto mesh = constructor.make_mesh(cloud);
+
+  io::write_mesh(mesh);
 }
