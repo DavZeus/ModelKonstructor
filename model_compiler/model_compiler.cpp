b3aa0d85661f9bc786ca100442419daf241834ae
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2021-07-23 21:32:26 +0300

    feat: add status printing

--- model_compiler/model_compiler.cpp
+++ model_compiler/model_compiler.cpp
@@ -4,16 +4,24 @@
 #include "io_operations.h"
 #include "model_constructor.h"
 
+#ifndef FMT_UNICODE
+#define FMT_UNICODE 1
+#endif
+#include <fmt/core.h>
+
 auto mc::make_rotation_model(const std::string &folder,
                              const model_constructor::methods method,
                              const model_constructor::additional_options option,
                              const unsigned step_count) -> void {
   const auto cloud = data_extractor::form_rotation(folder, step_count);
 
+  fmt::print("Считано {} точек\n", cloud.number_of_points());
+
   const model_constructor constructor{method, option};
   const auto mesh = constructor.make_mesh(cloud);
 
-  io::write_mesh(mesh);
+  const auto file = io::write_mesh(mesh);
+  fmt::print("Модель сохранена в файл {}\n", file);
 }
 
 auto mc::make_surface_model(const std::string &folder,
@@ -22,8 +30,11 @@ auto mc::make_surface_model(const std::string &folder,
                             const float step_length) -> void {
   const auto cloud = data_extractor::form_surface(folder, step_length);
 
+  fmt::print("Считано {} точек\n", cloud.number_of_points());
+
   const model_constructor constructor{method, option};
   const auto mesh = constructor.make_mesh(cloud);
 
-  io::write_mesh(mesh);
+  const auto file = io::write_mesh(mesh);
+  fmt::print("Модель сохранена в файл {}\n", file);
 }
